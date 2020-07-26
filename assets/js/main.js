a985bde30e306f95208b10f8197155546d7d7686
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-07-26 09:42:38 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,12 @@ import { SaveBlock } from './block-save/block-save';
 import { SaveFilters } from './save-filters/index.js';
 import BlockRegister from './block-register';
 
-jQuery( document ).ready(function(){
-  new SaveBlock();
-  new SaveFilters();
-  new BlockRegister();
-});
+if(wp.data) {
+
+	jQuery( document ).ready(function(){
+	  new SaveBlock();
+	  new SaveFilters();
+	  new BlockRegister();
+	});
+	
+}
